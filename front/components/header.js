15b6b2bd8853c5c9b90054c006cb2b9f47d34809
Sylvain Nieuwlandt <s.nieuwlandt.dev@gmail.com>
2019-05-21 15:07:48 +0200

    Create the error page skeleton & extract the header component to its own dedicated js

---
+++ front/components/header.js
@@ -0,0 +1,88 @@
+const headerTemplateContent = `
+<link rel="stylesheet" 
+        href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" 
+        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" 
+        crossorigin="anonymous">
+<link rel="import" href="components/main-menu.html">
+<style>                 
+    .fas {
+        font-size: 20px;
+    }
+
+    #header {
+        text-align: center;
+        margin-left: auto;
+        margin-right: auto;
+        font-size: 14px;
+        font-style: italic;
+        padding-top: 10px;
+        padding-bottom: 10px;
+    }
+
+    #header > div {
+        float: left;
+        width: 33%;
+    }
+
+    #header .fas:hover {
+        cursor: pointer;
+    }
+
+    #header-menu {
+        width: 130px;
+        margin-left: auto;
+        margin-right: 0;
+        text-align: right;
+    }
+
+    #header-menu > i {
+        margin: 5px;
+    }        
+
+    .main {
+        font-size: 24px;
+        font-weight: bold;
+        font-style: normal;
+    }
+
+    #front-text {
+        color: #444444;
+    }
+
+        #front-text:hover {
+        cursor:pointer;
+    }
+
+</style>
+<div id="header">
+        <ssd-main-menu></ssd-main-menu>
+        <div id="front-text">
+            <div class="main">La Salle sur Demande</div>
+            <div>Vendeur de rêve depuis 1999</div>
+        </div>
+        <div id="header-menu">
+            <i class="fas fa-shopping-cart front-text"></i>
+            <i class="fas fa-user-circle front-text" style="visibility: hidden"></i>
+            <i class="fas fa-search front-text" style="visibility:hidden"></i>
+        </div>
+</div>
+`;
+
+var parentDoc = document.currentScript.ownerDocument;
+window.customElements.define('ssd-header', class extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({mode: 'open'});
+        this.template = document.createElement('template');
+        this.template.innerHTML = headerTemplateContent;
+    }
+    connectedCallback() {
+        var clone = document.importNode(this.template.content, true);
+        var frontText = clone.querySelector("#front-text");
+        frontText.addEventListener('click', () => {
+            console.log("test")
+            window.location = '/'
+        });
+        this.shadowRoot.appendChild(clone);
+    }
+});
--- front/components/product.js
+++ front/components/product.js
@@ -1,116 +1,115 @@
-const templateContent = `
-    <style>                
-        #product {
-            width: 230px;
-            margin: 5px;
-            float: left;
-            padding: 5px 10px;
-            font-size: 13px;
-            border: solid thin #c0c0c0;
-            border-radius: 4px;
-        }
+const productTemplateContent = `
+<style>                
+    #product {
+        width: 230px;
+        margin: 5px;
+        float: left;
+        padding: 5px 10px;
+        font-size: 13px;
+        border: solid thin #c0c0c0;
+        border-radius: 4px;
+    }
 
-        #product:hover {
-            cursor: pointer;
-        }
+    #product:hover {
+        cursor: pointer;
+    }
 
-        #product div {
-            text-align: center;
-            width: 100%;
-            margin: 0;
-        }
+    #product div {
+        text-align: center;
+        width: 100%;
+        margin: 0;
+    }
 
-        #product img {
-            margin-left: auto;
-            margin-right: auto;
-            display: block;
-        }
-        #product .desc {
-            margin-left: auto;
-            margin-right: auto;
-            display:none;
-            width: 200px;
-            height: 150px;
-            font-size: 10px;
-        }
+    #product img {
+        margin-left: auto;
+        margin-right: auto;
+        display: block;
+    }
+    #product .desc {
+        margin-left: auto;
+        margin-right: auto;
+        display:none;
+        width: 200px;
+        height: 150px;
+        font-size: 10px;
+    }
 
-        #price {
-            font-weight: bold;
-            color: black;
-            margin-top: 10px;
-            font-size: 18px;
-        }
-    </style>
+    #price {
+        font-weight: bold;
+        color: black;
+        margin-top: 10px;
+        font-size: 18px;
+    }
+</style>
 
 
-    <div id="product">
-        <div>
-            <img id="img"
-                height="150"
-                width = "200"
-                src="" />
-            <div id="desc" style="display:none">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quam sapien, 
-                porta et odio at, iaculis luctus dui. Integer laoreet dolor turpis, nec aliquet 
-                eros mattis faucibus. Quisque turpis duis.
-            </div>
+<div id="product">
+    <div>
+        <img id="img"
+            height="150"
+            width = "200"
+            src="" />
+        <div id="desc" style="display:none">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quam sapien, 
+            porta et odio at, iaculis luctus dui. Integer laoreet dolor turpis, nec aliquet 
+            eros mattis faucibus. Quisque turpis duis.
         </div>
-        <div id="name"></div>
-        <div id="price"></div>
     </div>
+    <div id="name"></div>
+    <div id="price"></div>
+</div>
 `;
 
 var parentDoc = document.currentScript.ownerDocument;
-    window.customElements.define('ssd-product', class extends HTMLElement {
-        get name() {
-            return this.hasAttribute('name')
-        }
+window.customElements.define('ssd-product', class extends HTMLElement {
+    get name() {
+        return this.hasAttribute('name')
+    }
 
-        set name(val) {
-            this.setAttribute('name', val)
-        }
+    set name(val) {
+        this.setAttribute('name', val)
+    }
 
-        get price() {
-            return this.hasAttribute('price')
-        }
+    get price() {
+        return this.hasAttribute('price')
+    }
 
-        set price(val) {
-            this.setAttribute('price', val)
-        }
+    set price(val) {
+        this.setAttribute('price', val)
+    }
 
-        get imgLink() {
-            return this.hasAttribute('imgLink')
-        }
+    get imgLink() {
+        return this.hasAttribute('imgLink')
+    }
 
-        set imgLink(val) {
-            this.setAttribute('imgLink', val)
-        }
-        
-        constructor() {
-            super()
-            this.addEventListener('click', () => {
-                var code = this.getAttribute('code');
-                window.location = "/product_page.html?ref=" + code;
-            });
-            this.attachShadow({mode: 'open'});
-            this.template = document.createElement('template');
-            this.template.innerHTML = templateContent;
-        }
-        connectedCallback() { 
-                var clone = document.importNode(this.template.content, true);
-                console.log(clone);
-                
-                var name = this.getAttribute("name");
-                var imgLink = this.getAttribute("imgLink");
-                if (imgLink === undefined || imgLink == null) {
-                    imgLink = "https://www.novovelo.com/wp-content/themes/claue/assets/images/placeholder.png";
-                }
-                var price = this.getAttribute("price");
+    set imgLink(val) {
+        this.setAttribute('imgLink', val)
+    }
+    
+    constructor() {
+        super()
+        this.addEventListener('click', () => {
+            var code = this.getAttribute('code');
+            window.location = "/product_page.html?ref=" + code;
+        });
+        this.attachShadow({mode: 'open'});
+        this.template = document.createElement('template');
+        this.template.innerHTML = productTemplateContent;
+    }
+    connectedCallback() { 
+            var clone = document.importNode(this.template.content, true);
+            
+            var name = this.getAttribute("name");
+            var imgLink = this.getAttribute("imgLink");
+            if (imgLink === undefined || imgLink == null) {
+                imgLink = "https://www.novovelo.com/wp-content/themes/claue/assets/images/placeholder.png";
+            }
+            var price = this.getAttribute("price");
 
-                clone.querySelector("#name").innerHTML = name;
-                clone.querySelector("#img").src = imgLink;
-                clone.querySelector("#price").innerHTML = price;
+            clone.querySelector("#name").innerHTML = name;
+            clone.querySelector("#img").src = imgLink;
+            clone.querySelector("#price").innerHTML = price;
 
-                this.shadowRoot.appendChild(clone);
-        }
-    });
+            this.shadowRoot.appendChild(clone);
+    }
+});
