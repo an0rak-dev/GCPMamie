f6d786c64b76ff9a7545e521a116d8a7dcb77c43
Sylvain Nieuwlandt <s.nieuwlandt.dev@gmail.com>
2019-05-23 20:35:28 +0200

    Fill db with dummy infos

---
+++ datas/02_load-products.sql
@@ -0,0 +1,31 @@
+INSERT INTO ssd_prod.products VALUES (
+	'p01', 'Half-Life 3', 45.99
+);
+
+INSERT INTO ssd_prod.products VALUES (
+	'p02', 'Le Film Kaamelott', 21.99
+);
+
+INSERT INTO ssd_prod.products VALUES (
+	'p03', 'Nez de Voldemort', 30.0
+);
+
+INSERT INTO ssd_prod.products VALUES (
+	'p04', 'Anneau Unique', 9.99
+);
+
+INSERT INTO ssd_prod.products VALUES (
+	'p05', 'The Elder Scrolls VI', 59.08
+);
+
+INSERT INTO ssd_prod.products VALUES (
+	'p06', 'Gant de l''infini', 2000.99
+);
+
+INSERT INTO ssd_prod.products VALUES (
+	'p07', 'Stargate The Game', 68.99
+);
+
+INSERT INTO ssd_prod.products VALUES (
+	'p08', 'Apple AirPower', 2000.99
+);
---
+++ datas/03_load-stocks.sql
@@ -0,0 +1,31 @@
+INSERT INTO ssd_prod.stocks VALUES (
+	'p01', 99
+);
+
+INSERT INTO ssd_prod.stocks VALUES (
+	'p02', 23
+);
+
+INSERT INTO ssd_prod.stocks VALUES (
+	'p03', 1
+);
+
+INSERT INTO ssd_prod.stocks VALUES (
+	'p04', 57
+);
+
+INSERT INTO ssd_prod.stocks VALUES (
+	'p05', 99
+);
+
+INSERT INTO ssd_prod.stocks VALUES (
+	'p06', 4
+);
+
+INSERT INTO ssd_prod.stocks VALUES (
+	'p07', 99
+);
+
+INSERT INTO ssd_prod.stocks VALUES (
+	'p08', 183
+);
