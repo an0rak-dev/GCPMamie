1d3f93779b4f9355a49b5b36d6f298b42dbeb095
Sylvain Nieuwlandt <s.nieuwlandt.dev@gmail.com>
2019-06-01 01:17:10 +0200

    Add the pubsub<->batch Python connector

---
+++ gcp/batch/pubsub_connector.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import socket
+import time
+from google.cloud import pubsub_v1
+
+project_id='talk-gcp-mamie'
+subscription_id='nextOrder'
+
+def callback(msg):
+    data = msg.data
+    msg.ack()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('127.0.0.1', 10022))
+    s.send(data)
+    s.close()
+
+
+def main():
+    subscriber = pubsub_v1.SubscriberClient()
+    subscriptionPath = subscriber.subscription_path(project_id, subscription_id)
+    
+    subscriber.subscribe(subscriptionPath, callback=callback)
+    while True:
+        time.sleep(60)
+
+if __name__ == '__main__':
+    main()
